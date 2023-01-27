ff8c8bd6a8a4b43f5af5c135cc76c124253e4561
Harsh <harshnag23@gmail.com>
2023-01-27 20:21:24 +0530

    Setup for deploying the contract is done

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
