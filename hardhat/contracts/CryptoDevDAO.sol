5de7d0b444ba42fb90da65a5d01be18516452f80
Harsh <harshnag23@gmail.com>
2023-01-27 18:09:01 +0530

    Contract complted

--- hardhat/contracts/CryptoDevDAO.sol
+++ hardhat/contracts/CryptoDevDAO.sol
@@ -13,6 +13,10 @@ struct Proposal {
     bool executed;
     mapping(uint256 => bool) voters;
 }
+enum Vote {
+    YAY,
+    NAY
+}
 
 contract CryptoDevDAO is Ownable {
     mapping(uint256 => Proposal) public proposals;
@@ -23,6 +27,24 @@ contract CryptoDevDAO is Ownable {
 
     modifier nftHolderOnly() {
         require(cryptoDevDAO.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline > block.timestamp,
+            "DEADLINE EXCEEDED"
+        );
+        _;
+    }
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(
+            proposals[proposalIndex].deadline <= block.timestamp,
+            "DEADLINE NOT EXCEEDED"
+        );
+        require(
+            proposals[proposalIndex].executed == false,
+            "PROPOSAL_ALREADY_EXECUTED"
+        );
         _;
     }
 
@@ -43,4 +65,52 @@ contract CryptoDevDAO is Ownable {
         numProposals++;
         return numProposals - 1;
     }
+
+    function voteOnProposal(uint256 proposalIndex, Vote vote)
+        external
+        nftHolderOnly
+        activeProposalOnly(proposalIndex)
+    {
+        Proposal storage proposal = proposals[proposalIndex];
+        uint256 voterNFTBalance = cryptoDevDAO.balanceOf(msg.sender);
+        uint256 numVotes = 0;
+
+        for (uint256 i = 0; i < voterNFTBalance; i++) {
+            uint256 tokenId = cryptoDevDAO.tokenOfOwnerByIndex(msg.sender, i);
+            if (proposal.voters[tokenId] == false) {
+                numVotes++;
+                proposal.voters[tokenId] = true;
+            }
+        }
+        require(numVotes > 0, "ALREADY VOTED");
+        if (vote == Vote.YAY) {
+            proposal.yayVotes += numVotes;
+        } else {
+            proposal.nayVotes += numVotes;
+        }
+    }
+
+    function executeProposal(uint256 proposalIndex)
+        external
+        nftHolderOnly
+        inactiveProposalOnly(proposalIndex)
+    {
+        Proposal storage proposal = proposals[proposalIndex];
+        if (proposal.yayVotes > proposal.nayVotes) {
+            uint256 nftPrice = nftMarketPlace.getPrice();
+            require(address(this).balance >= nftPrice, "NOT ENOUGH FUNDS");
+            nftMarketPlace.purchase{value: nftPrice}(proposal.nftTokenId);
+        }
+        proposal.executed = true;
+    }
+
+    function withdrawEther() external onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "Nothing to withdraw; contract balance empty");
+        payable(owner()).transfer(amount);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
